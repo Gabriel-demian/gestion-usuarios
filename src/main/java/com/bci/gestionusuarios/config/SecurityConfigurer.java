f78a605c0075f7346c31f6d2671624f38907d59f
Liliherax <57782295+Gabriel-demian@users.noreply.github.com>
2023-10-24 14:57:16 -0300

    fixed error with secConfig.

--- src/main/java/com/bci/gestionusuarios/config/SecurityConfigurer.java
+++ src/main/java/com/bci/gestionusuarios/config/SecurityConfigurer.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -22,6 +23,7 @@ public class SecurityConfigurer {
 		http.csrf().disable(); // why??
 		http.authorizeHttpRequests().requestMatchers(unauthorizedEndpointsMatcher).permitAll()
 				.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
